6385f27d3f111813c3fa0a5514e7f19bb21d232a
EC2 Default User <ec2-user@ip-172-31-2-13.eu-west-1.compute.internal>
2019-09-22 17:08:36 +0000

    Basic login functionality

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include("common/php_scripts/check_cookie.php"); ?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>College Placement</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+  <br />
+  <div class="container">
+   <h2 align="center">Welcome to College Placement</h2>
+   <br />
+   <?php include("./common/html_partials/header.html"); ?>
+   <?php include("./common/php_scripts/welcome.php"); ?>
+  </div>
+ </body>
+</html>
+
